22ee5be62c27c9b85d07508d8293093c596ea9fe
nyaomaru <nyaonyao0725@gmail.com>
2024-05-03 08:35:24 +0900

    Add login post

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-'use strict';
+"use strict";
 
 // // Import the required modules
-var express = require('express');
-var cors = require('cors');
+var express = require("express");
+var cors = require("cors");
 
 // Import the Login controller
-var loginController = require('./controllers/Login');
+var loginController = require("./controllers/Login");
 
 // Create an Express application
 var app = express();
@@ -13,14 +13,15 @@ var app = express();
 // Add CORS middleware to handle CORS headers
 app.use(cors());
 
-app.get('/login', loginController.loginGET);
+app.get("/login", loginController.loginGET);
+app.post("/login", loginController.loginPOST);
 
 // Handle OPTIONS requests explicitly
-app.options('*', cors()); // Enable pre-flight across all routes
+app.options("*", cors()); // Enable pre-flight across all routes
 
 // Start the server
 var server = app.listen(8080, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log("Server listening at http://%s:%s", host, port);
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log("Server listening at http://%s:%s", host, port);
 });
